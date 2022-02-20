30c63a9c25e2e7b33a24ea15fb6bcb7f9ca25687
Shuji Shigenobu <sshigenobu@gmail.com>
2022-02-20 14:07:59 +0900

    thor was introduced to enhance the functionality and to increase the development efficiency of CLI client.

--- openbd_rbcli.rb
+++ openbd_rbcli.rb
@@ -1,25 +1,54 @@
 require 'open-uri'
 require 'json'
+require 'thor'
 
-baseurl = "https://api.openbd.jp/v1"
+module OpenBd
 
-query = ARGV[0]
-q = query.strip
-raise unless /^\d+$/.match(q)
+  BASE_API_URL = "https://api.openbd.jp/v1"
 
-mode = (ARGV[1] || "summary") # summary or full
+  class CLI < Thor
 
-url = nil
-url = baseurl + "/get?isbn=#{q}"
+    ## hack to enable -h option
+    ## ref: https://magazine.rubyist.net/articles/0046/0046-Milkode.html#%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%9E%E3%82%A4%E3%82%BA-milk-add--h-%E3%82%92%E5%AE%9F%E7%8F%BE%E3%81%99%E3%82%8B
 
-uri = URI.parse(url)
-# STDERR.puts uri
+    class_option :help, :type => :boolean, :aliases => '-h', :desc => 'Help message'
+    
+    no_tasks do
+      def invoke_command(task, *args)
+        if options[:help]
+          OpenBd::CLI.task_help(shell, task.name)
+        else
+          super
+        end
+      end
+    end
+    ## end of -h option setting
+    
+    desc "get ISBN", "retrieve book information by ISBN."
+    option :mode, :aliases => '-m', :enum => ["summary", "full"], :default => "summary", :desc => "output mode"
+    def get(query)
+      q = query.strip
+      raise unless /^\d+$/.match(q)
+      mode = options[:mode]
 
-res = uri.open.read
+      url = nil
+      url = BASE_API_URL + "/get?isbn=#{q}"
 
-data = JSON.parse(res) #data : Array
-if mode == 'summary'
-  puts  data.map{|e| e["summary"]}.to_json
-else
-  puts res
+      uri = URI.parse(url)
+      STDERR.puts uri
+
+      res = uri.open.read
+
+      data = JSON.parse(res) #data : Array
+      if mode == 'summary'
+        puts  data.map{|e| e["summary"]}.to_json
+      else 
+        puts res
+      end
+    end
+
+  end
+
 end
+
+OpenBd::CLI.start(ARGV)
